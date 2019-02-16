23197fc1b731e351c1e68223054d4d3e2801d425
jki14 <jki14wz@gmail.com>
2019-02-16 21:19:32 +0900

    Topcoder Arena Plugin - Greed Configure Update
    1. Add greed_template.py
    2. Update greed.conf
    3. Test configure changes with SRM 749 Div2 pt250

---
+++ greed/greed_template.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import array
+import bisect
+import collections
+import fractions
+import heapq
+import itertools
+import math
+import re
+import string
+
+class joshu:
+
+  # stdio
+  @staticmethod
+  def getInt():
+    return int(raw_input().strip())
+  @staticmethod
+  def getInts():
+    return [int(i) for i in raw_input().strip().split()]
+  @staticmethod
+  def getFloat():
+    return float(raw_input().strip())
+  @staticmethod
+  def getFloats():
+    return [float(i) for i in raw_input().strip().split()]
+  @staticmethod
+  def getStr():
+    return str(raw_input().strip())
+  @staticmethod
+  def getStrs():
+    return [str(i) for i in raw_input().strip().split()]
+
+class ${ClassName}:
+  def ${Method.Name}(self, ${Method.Params}):
+    return ${Method.ReturnType;zeroval}
+
+${CutBegin}
+${<TestCode}
+${CutEnd}
