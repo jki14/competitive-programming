c4f9a4470c02bd726b881e1f6bb60eb4f982944a
jki14 <jki14wz@gmail.com>
2019-03-17 23:07:37 +0900

    [WIP] Implement joshu
    1. [WIP] Implement joshu::imod_t, update

--- joshu.cpp
+++ joshu.cpp
@@ -3,23 +3,22 @@
 namespace joshu {
 
 inline namespace {
-
 template<int64_t token, int64_t token2>
 struct token_compatible {
   constexpr static bool value
     = token2 % token == 0;
 };
 
-template<int64_t token, typename INT>
+template<int64_t token, typename integral>
 struct add_safe {
   constexpr static bool value
-    = std::numeric_limits<INT>::max() - (token - 1) >= (token - 1);
+    = std::numeric_limits<integral>::max() - (token - 1) >= (token - 1);
 };
 
-template<int64_t token, typename INT>
+template<int64_t token, typename integral>
 struct mul_safe {
   constexpr static bool value
-    = std::numeric_limits<INT>::max() / (token - 1) >= (token - 1);
+    = std::numeric_limits<integral>::max() / (token - 1) >= (token - 1);
 };
 
 template<int64_t token>
@@ -37,7 +36,12 @@ public:
     foo_ = rhs % token;
   }
 
-  imod_t operator+(imod_t const& rhs) const {
+  template<int64_t token2>
+  typename std::enable_if<
+    token_compatible<token, token2>::value
+    && add_safe<token2, foo_t>::value
+    , imod_t<token>&>::type
+  imod_t<token> operator+(imod_t<token2> const& rhs) const {
     imod_t bar(*this);
     bar += rhs;
     return bar;
@@ -93,12 +97,10 @@ private:
   template<int64_t token2>
   friend class imod_t;
   foo_t foo_ = 0;
-
 };
-
 }
 
-  //TODO
+//TODO
 
 }
 
@@ -111,5 +113,6 @@ int main() {
   joshu::imod_t<9223372036854775807LL> mit3 = -2;
   mit3 += mit3;
   mit3 *= mit3;
+  mit += 3;
   return 0;
 }
