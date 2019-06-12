0851b9d7b732df2c47f4ad409a2793425956317c
jki14 <jki14wz@gmail.com>
2019-06-12 21:32:02 +0900

    Training Contest In-game Commit
    1. Training Contest: Warming Up #2 FOR TOPCODER OPEN 2019 CHINA
     * In-game codes commit
     * Cloned from TCO18 Beijing

---
+++ greed/TCO18 Beijing/JumpingJackDiv1.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+import array
+import bisect
+import collections
+import fractions
+import heapq
+import itertools
+import math
+import re
+import string
+import sys
+
+class JumpingJackDiv1:
+  def getLocationOfJack(self, dist, k, n):
+    return dist * (n / k * (k - 1) + n % k)
+
+# CUT begin
+# TEST CODE FOR PYTHON {{{
+import sys, time, math
+
+def tc_equal(expected, received):
+    try:
+        _t = type(expected)
+        received = _t(received)
+        if _t == list or _t == tuple:
+            if len(expected) != len(received): return False
+            return all(tc_equal(e, r) for (e, r) in zip(expected, received))
+        elif _t == float:
+            eps = 1e-9
+            d = abs(received - expected)
+            return not math.isnan(received) and not math.isnan(expected) and d <= eps * max(1.0, abs(expected))
+        else:
+            return expected == received
+    except:
+        return False
+
+def pretty_str(x):
+    if type(x) == str:
+        return '"%s"' % x
+    elif type(x) == tuple:
+        return '(%s)' % (','.join( (pretty_str(y) for y in x) ) )
+    else:
+        return str(x)
+
+def do_test(dist, k, n, __expected):
+    startTime = time.time()
+    instance = JumpingJackDiv1()
+    exception = None
+    try:
+        __result = instance.getLocationOfJack(dist, k, n);
+    except:
+        import traceback
+        exception = traceback.format_exc()
+    elapsed = time.time() - startTime   # in sec
+
+    if exception is not None:
+        sys.stdout.write("RUNTIME ERROR: \n")
+        sys.stdout.write(exception + "\n")
+        return 0
+
+    if tc_equal(__expected, __result):
+        sys.stdout.write("PASSED! " + ("(%.3f seconds)" % elapsed) + "\n")
+        return 1
+    else:
+        sys.stdout.write("FAILED! " + ("(%.3f seconds)" % elapsed) + "\n")
+        sys.stdout.write("           Expected: " + pretty_str(__expected) + "\n")
+        sys.stdout.write("           Received: " + pretty_str(__result) + "\n")
+        return 0
+
+def run_tests():
+    sys.stdout.write("JumpingJackDiv1 (250 Points)\n\n")
+
+    passed = cases = 0
+    case_set = set()
+    for arg in sys.argv[1:]:
+        case_set.add(int(arg))
+
+    with open("JumpingJackDiv1.sample", "r") as f:
+        while True:
+            label = f.readline()
+            if not label.startswith("--"): break
+
+            dist = int(f.readline().rstrip())
+            k = int(f.readline().rstrip())
+            n = int(f.readline().rstrip())
+            f.readline()
+            __answer = int(f.readline().rstrip())
+
+            cases += 1
+            if len(case_set) > 0 and (cases - 1) in case_set: continue
+            sys.stdout.write("  Testcase #%d ... " % (cases - 1))
+            passed += do_test(dist, k, n, __answer)
+
+    sys.stdout.write("\nPassed : %d / %d cases\n" % (passed, cases))
+
+    T = time.time() - 1560338216
+    PT, TT = (T / 60.0, 75.0)
+    points = 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT))
+    sys.stdout.write("Time   : %d minutes %d secs\n" % (int(T/60), T%60))
+    sys.stdout.write("Score  : %.2f points\n" % points)
+
+if __name__ == '__main__':
+    run_tests()
+
+# }}}
+# CUT end
