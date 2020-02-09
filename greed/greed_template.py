23535bc31476846f80b97c4e9152f48136310053
jki14 <jki14wz@gmail.com>
2020-02-09 23:21:03 +0900

    Change default python indent to 4 whitespaces
    1. Change default python indent to 4 whitespaces from 2, which includes
    joshu.py and greed/greed_template.py

--- greed/greed_template.py
+++ greed/greed_template.py
@@ -11,8 +11,8 @@ import string
 import sys
 
 class ${ClassName}:
-  def ${Method.Name}(self, ${Method.Params}):
-    return ${Method.ReturnType;zeroval}
+    def ${Method.Name}(self, ${Method.Params}):
+        return ${Method.ReturnType;zeroval}
 
 ${CutBegin}
 ${<TestCode}
--- joshu.py
+++ joshu.py
@@ -12,29 +12,29 @@ import sys
 
 class joshu:
 
-  # stdio
-  @staticmethod
-  def getInt():
-    return int(raw_input().strip())
-  @staticmethod
-  def getInts():
-    return [int(i) for i in raw_input().strip().split()]
-  @staticmethod
-  def getFloat():
-    return float(raw_input().strip())
-  @staticmethod
-  def getFloats():
-    return [float(i) for i in raw_input().strip().split()]
-  @staticmethod
-  def getStr():
-    return str(raw_input().strip())
-  @staticmethod
-  def getStrs():
-    return [str(i) for i in raw_input().strip().split()]
+    # stdio
+    @staticmethod
+    def getInt():
+        return int(raw_input().strip())
+    @staticmethod
+    def getInts():
+        return [int(i) for i in raw_input().strip().split()]
+    @staticmethod
+    def getFloat():
+        return float(raw_input().strip())
+    @staticmethod
+    def getFloats():
+        return [float(i) for i in raw_input().strip().split()]
+    @staticmethod
+    def getStr():
+        return str(raw_input().strip())
+    @staticmethod
+    def getStrs():
+        return [str(i) for i in raw_input().strip().split()]
 
 def main():
-  # TODO
-  pass
+    # TODO
+    pass
 
 if __name__ == '__main__':
-  main()
+    main()
