030bd1d7c9c0bd0da5ae890652c64450e24ac0d5
jki14 <jki14wz@gmail.com>
2020-02-17 19:18:38 +0900

    TC-SRM 777 Up-solving
    1. Up-solving Topcoder SRM 777 pt500

--- greed/SRM 777/StringTransformation.cpp
+++ greed/SRM 777/StringTransformation.cpp
@@ -1,44 +1,75 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-int n, m;
-int f[2600][2600], g[2600][2600];
+int f[2600][2600];
 
 class StringTransformation {
 public:
-  string getResult(string const& lhs, string const& rhs) {
-    n = lhs.length();
-    m = rhs.length();
+  string getResult(string const& lhs_, string const& rhs_) {
+    vector<pair<char, int>> lhs, rhs;
+    static const auto enroll = [](string const& foo,
+                                      vector<pair<char,int>> &bar) -> void {
+      bar.reserve(foo.length());
+      bar.clear();
+      for (auto const chr : foo) {
+        if (!bar.empty() && bar.back().first == chr) {
+          ++bar.back().second;
+        } else {
+          bar.emplace_back(chr, 1);
+        }
+      }
+    };
+    enroll(lhs_, lhs);
+    enroll(rhs_, rhs);
+    int n = lhs.size();
+    int m = rhs.size();
+    if (m == 1) {
+      return lhs_ == rhs_ ? "YES" : "NO";
+    }
     for (int i = 0; i <= n; ++i) {
       for (int j = 0; j <= m; ++j) {
         f[i][j] = 0;
-        g[i][j] = 0;
       }
     }
-    f[0][0] = 1;
-    for (int i = 1; i <= n; ++i) {
-      for (int j = 1; j <= m; ++j) {
-        if (lhs[i - 1] == rhs[j - 1]) {
-          f[i][j] |= f[i - 1][j - 1];
-          if (j > 1 && rhs[j - 1] != rhs[j - 2]) {
-            f[i][j] |= g[i - 1][j - 1];
+    static const auto match = [](pair<char, int> const& lhs,
+                                     pair<char, int> const& rhs) -> int {
+      if (lhs.first != rhs.first) return 0;
+      if (lhs.second < rhs.second) return 0;
+      if ((lhs.second & 1) != (rhs.second & 1)) return 0;
+      return 1;
+    };
+    static const auto bic = [](char const c) -> int {
+      if (c == 'R') return 1;
+      if (c == 'G') return 2;
+      return 4;
+    };
+    f[1][1] = match(lhs.front(), rhs.front());
+    for (int j = 1; j < m; ++j) {
+      int mask = 0;
+      for (int i = 1; i < n; ++i) {
+        if (lhs[i - 1].second & 1) {
+          mask = 0;
+        } else if (mask) {
+          mask |= bic(lhs[i - 1].first);
+        }
+        if (f[i][j]) {
+          if (match(lhs[i], rhs[j])) f[i + 1][j + 1] = 1;
+          if (!mask) {
+            mask = bic(lhs[i - 1].first);
           }
-          // fprintf(stderr, "f[%d][%d] = %d\n", i, j, f[i][j]);
-        }
-        if (i > 2 && lhs[i - 1] == lhs[i - 2]) {
-          g[i][j] |= f[i - 2][j];
-          g[i][j] |= g[i - 2][j];
-          // fprintf(stderr, "g[%d][%d] = %d\n", i, j, g[i][j]);
+        };
+        // fprintf(stderr, "f[%d][%d] = %d, mask = %d\n", i, j, f[i][j], mask);
+        if (match(lhs[i], rhs[j]) && (mask | bic(lhs[i].first)) == 7) {
+          f[i + 1][j + 1] = 1;
         }
       }
     }
-    if (f[n][m]) return "YES";
-    return "NO";
+    return f[n][m] ? "YES" : "NO";
   }
 };
 
 int main() {
   StringTransformation foo;
-  assert("YES" == foo.getResult("RGGG", "RG"));
+  assert("YES" == foo.getResult("RGBRGGRRBGB", "RGBRBGB"));
   return 0;
 }
