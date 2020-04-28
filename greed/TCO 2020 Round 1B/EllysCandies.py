3723f1a0658d0cbc78bd0962b9e56d5d39f08688
jki14 <jki14wz@gmail.com>
2020-04-28 22:23:41 +0900

    TCO 2020 Round1B In-game Commit
    TCO 2020 Round1B Round1B In-game Codes.

---
+++ greed/TCO 2020 Round 1B/EllysCandies.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+import array
+import bisect
+import collections
+import fractions
+import heapq
+import itertools
+import math
+import re
+import string
+import sys
+
+class EllysCandies:
+    def getWinner(self, boxes):
+        boxes = sorted(boxes)
+        n = len(boxes)
+        ate = [0, 0]
+        for i in xrange(n):
+            if ((n - i) & 1) == 1:
+                ate[i & 1] += boxes[-1]
+                boxes = [boxes[j] + boxes[-1] for j in xrange(n - i - 1)]
+            else:
+                ate[i & 1] += boxes[0]
+                boxes = [boxes[j] + boxes[0] for j in xrange(1, n - i)]
+        if ate[0] != ate[1]:
+            if ate[0] > ate[1]:
+                return "Elly"
+            else:
+                return "Kris"
+        else:
+            return "Draw"
+
+# CUT begin
+# TEST CODE FOR PYTHON {{{
+import sys, time, math
+
+def tc_equal(expected, received):
+    try:
+        _t = type(expected)
+        received = _t(received)
+        if _t == list or _t == tuple:
+            if len(expected) != len(received): return False
+            return all(tc_equal(e, r) for (e, r) in zip(expected, received))
+        elif _t == float:
+            eps = 1e-9
+            d = abs(received - expected)
+            return not math.isnan(received) and not math.isnan(expected) and d <= eps * max(1.0, abs(expected))
+        else:
+            return expected == received
+    except:
+        return False
+
+def pretty_str(x):
+    if type(x) == str:
+        return '"%s"' % x
+    elif type(x) == tuple:
+        return '(%s)' % (','.join( (pretty_str(y) for y in x) ) )
+    else:
+        return str(x)
+
+def do_test(boxes, __expected):
+    startTime = time.time()
+    instance = EllysCandies()
+    exception = None
+    try:
+        __result = instance.getWinner(boxes);
+    except:
+        import traceback
+        exception = traceback.format_exc()
+    elapsed = time.time() - startTime   # in sec
+
+    if exception is not None:
+        sys.stdout.write("RUNTIME ERROR: \n")
+        sys.stdout.write(exception + "\n")
+        return 0
+
+    if tc_equal(__expected, __result):
+        sys.stdout.write("PASSED! " + ("(%.3f seconds)" % elapsed) + "\n")
+        return 1
+    else:
+        sys.stdout.write("FAILED! " + ("(%.3f seconds)" % elapsed) + "\n")
+        sys.stdout.write("           Expected: " + pretty_str(__expected) + "\n")
+        sys.stdout.write("           Received: " + pretty_str(__result) + "\n")
+        return 0
+
+def run_tests():
+    sys.stdout.write("EllysCandies (300 Points)\n\n")
+
+    passed = cases = 0
+    case_set = set()
+    for arg in sys.argv[1:]:
+        case_set.add(int(arg))
+
+    with open("EllysCandies.sample", "r") as f:
+        while True:
+            label = f.readline()
+            if not label.startswith("--"): break
+
+            boxes = []
+            for i in range(0, int(f.readline())):
+                boxes.append(int(f.readline().rstrip()))
+            boxes = tuple(boxes)
+            f.readline()
+            __answer = f.readline().rstrip()
+
+            cases += 1
+            if len(case_set) > 0 and (cases - 1) in case_set: continue
+            sys.stdout.write("  Testcase #%d ... " % (cases - 1))
+            passed += do_test(boxes, __answer)
+
+    sys.stdout.write("\nPassed : %d / %d cases\n" % (passed, cases))
+
+    T = time.time() - 1588072185
+    PT, TT = (T / 60.0, 75.0)
+    points = 300 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT))
+    sys.stdout.write("Time   : %d minutes %d secs\n" % (int(T/60), T%60))
+    sys.stdout.write("Score  : %.2f points\n" % points)
+
+if __name__ == '__main__':
+    run_tests()
+
+# }}}
+# CUT end
