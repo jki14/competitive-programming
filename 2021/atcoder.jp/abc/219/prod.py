39ed1b853d7c110aaf757f72645d7cb01a4f79ce
jki14 <jki14wz@gmail.com>
2021-11-20 21:42:59 +0900

    AtCoder Beginner Contest 219 Optimization Up-solving Commit
    1. Optimize the memory usage of Problem-D.

--- 2021/atcoder.jp/abc/219/prod.py
+++ 2021/atcoder.jp/abc/219/prod.py
@@ -5,22 +5,22 @@ def solution(a, x, y):
     inf = 0x3f3f3f3f
 
     n = len(a)
-    f = [[[inf for k in xrange(y + 1)] for j in xrange(x + 1)] for i in xrange(n + 1)]
-    f[0][0][0] = 0
+    f = [[inf for k in xrange(y + 1)] for j in xrange(x + 1)]
+    f[0][0] = 0
 
     for i in xrange(n):
+        g = f
+        f = [[inf for k in xrange(y + 1)] for j in xrange(x + 1)]
         for j in xrange(x + 1):
             for k in xrange(y + 1):
-                if f[i][j][k] >= inf:
+                if g[j][k] >= inf:
                     continue
-                # stderr.write('< f[%d][%d][%d] = %d\n' % (i, j, k, f[i][j][k]))
-                f[i + 1][j][k] = min(f[i][j][k], f[i + 1][j][k])
+                f[j][k] = min(g[j][k], f[j][k])
                 p = min(j + a[i][0], x)
                 q = min(k + a[i][1], y)
-                f[i + 1][p][q] = min(f[i][j][k] + 1, f[i + 1][p][q])
-                # stderr.write('f[%d][%d][%d] = %d\n' % (i + 1, p, q, f[i + 1][p][q]))
+                f[p][q] = min(g[j][k] + 1, f[p][q])
  
-    stdout.write(str(f[n][x][y] if f[n][x][y] < inf else -1) + '\n')
+    stdout.write(str(f[x][y] if f[x][y] < inf else -1) + '\n')
 
 
 def main():
